3b9a5c82e4efd711e49ce86f843b0ae74d3ae48d
AndersonnReinaldo <andersonnreinaldo@gmail.com>
2022-06-14 20:56:11 -0300

    finish libs

--- android/app/src/main/java/com/powermanagement/MainActivity.java
+++ android/app/src/main/java/com/powermanagement/MainActivity.java
@@ -1,9 +1,9 @@
 package com.powermanagement;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
 public class MainActivity extends ReactActivity {
 
   /**
@@ -13,6 +13,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "powerManagement";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
