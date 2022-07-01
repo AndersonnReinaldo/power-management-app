1e4151b4fb9d8d28605f0c58a570a61e1243ccab
Reinaldo <andersonnreinaldo@gmail.com>
2022-07-01 19:50:11 -0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useColorScheme, StatusBar,LogBox } from 'react-native'
 import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
@@ -8,6 +8,8 @@ import themes from './theme';
 import EnergyProvider from './context/EnergyProvider'
 
 import ignoreWarnings from 'ignore-warnings';
+import OneSignal from 'react-native-onesignal';
+
 ignoreWarnings('warn',['ViewPropTypes','[react-native-gesture-handler]'])
 LogBox.ignoreLogs([
     'ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from \'deprecated-react-native-prop-types\'.',
@@ -19,8 +21,11 @@ LogBox.ignoreLogs([
 const Index = () => { 
     //dark, light, null, undefined
     const deviceTheme = useColorScheme();
-
     const theme = themes[deviceTheme] || themes.dark
+
+    useEffect(() => {
+        OneSignal.setAppId('72536c65-5f7c-4ae5-b628-696004da3954');
+    },[]);
     
     return(
         <GestureHandlerRootView style={{ flex: 1 }}>
